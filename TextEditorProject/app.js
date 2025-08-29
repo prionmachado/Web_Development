c091e95f7ea74b4d63572614df32ee7e7996887c
prionmachado <prion2587@gmail.com>
2025-08-29 15:58:24 +0530

    Removed duplicate folders

--- TextEditorProject/app.js
+++
@@ -1,6 +0,0 @@
-let text = document.querySelector("#textInput");
-let para = document.querySelector("p");
-text.addEventListener("input", function(event) {
-    console.log(this.value);
-    para.innerText = this.value;
-}); 
