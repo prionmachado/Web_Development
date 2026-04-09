a2ca281ae023315d80e27eab1f3f5ed47b8fd319
prionmachado <prion2587@gmail.com>
2026-04-09 19:17:49 +0530

    Added Files

---
+++ NODE_CLASS/script.js
@@ -0,0 +1,14 @@
+import { sum, PI } from "./math.js";  
+
+console.log(sum(5,6)); 
+console.log(PI); 
+ 
+// console.log(mul(5,6));
+// console.log(g);
+// console.log(PI);
+
+// required whole fruits folder
+// const fruits = require("./fruits");
+// console.log(fruits);
+// console.log(fruits[0]);
+// console.log(fruits[1].name);  
